79327d2603c04aed4fd2d4045725a8f67545ce40
unknown <vitor_paulagomes@hotmail.com>
2022-11-06 19:30:21 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+              <img src="assests/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+              <strong>${hour}</strong>
+              <img src="assests/icon-${player2}.svg" alt="Bandeira do ${player2}" />
+            </li>
+  `
+}
+
+let delay = -0.3
+function createCard(date, day, games) {
+  delay = delay + 0.3
+  return `
+  <div class="card" style="animation-delay: ${delay}s">
+          <h2>${date} <span>${day}</span></h2>
+          <ul>
+            ${games}
+          </ul>
+        </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("brazil", "16:00", "serbia") +
+      createGame("portugal", "13:00", "ghana")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("brazil", "16:00", "cameroon") +
+      createGame("southkorea", "12:00", "portugal")
+  )
